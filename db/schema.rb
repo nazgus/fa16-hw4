90af4dadb42b0db09cb187fb5cfe40a7f1259d04
nazgus <gus@deleon.org.mx>
2016-10-11 11:20:01 -0700

    Rake todo migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011181321) do
+ActiveRecord::Schema.define(version: 20161011181748) do
 
   create_table "cats", force: true do |t|
     t.string "name"
+  end
+
+  create_table "todos", force: true do |t|
+    t.string   "tasks"
+    t.boolean  "finished"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
